5ba20441b1c06b0a1ac56ed0e06d308d480d2105
dinozavr2005 <66667779+dinozavr2005@users.noreply.github.com>
2022-04-29 12:07:12 +0300

    create password project

---
+++ Password/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Password
+//
+//  Created by Владимир on 29.04.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
